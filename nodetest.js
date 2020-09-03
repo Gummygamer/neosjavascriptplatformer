f78926d3c37a04b3988e27856614f80083aa121c
André Castro <aisushin@gmail.com>
2020-09-02 22:16:37 -0300

    Add files via upload

---
+++ nodetest.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var pos = require('./pos');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var actstr = req.url;
+  
+  switch(actstr) {
+	  case "/moveright":
+	     res.end(pos.moveRight());
+		 break;
+	  case "/moveleft":
+         res.end(pos.moveLeft());
+		 break;
+	  default:
+	     res.end(pos.fetchCoord());
+		 break;
+  }
+  
+}).listen(8080);
---
+++ pos.js
@@ -0,0 +1,48 @@
+var x = 0;
+var y = 5;
+var z = 0;
+
+dy = 0;
+ddy = 0;
+dx = 0;
+
+function updateCoord() {
+  dy = dy + ddy;
+  y = y + dy;
+  x = x + dx;
+  
+  dx = dx / 2;
+  
+  if (y <= 0) {
+     dy = 0;
+	 y = 0;
+	 ddy = 0;
+  }
+  
+  if(y > 0){
+     ddy = -0.00001
+  }
+}
+
+function getCoord() {
+  var coordstr = "[" + x.toString() + ";" + y.toString() + ";" + z.toString() + "]";
+  updateCoord();
+  return coordstr;
+}
+
+exports.fetchCoord = function () {
+  return getCoord();
+};
+
+exports.moveRight = function() {
+  dx = dx + 0.001;
+
+  return getCoord();
+}
+
+exports.moveLeft = function() {
+  dx = dx - 0.001;
+
+  return getCoord();
+}
+
